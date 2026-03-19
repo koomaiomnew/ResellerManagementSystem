a8984690fb424c002c69fa8a1a9bef91f851e958
PALATIP07 <66021836@up.ac.th>
2026-03-19 16:57:19 +0700

    update t

---
+++ backend/rms-backend/src/main/java/com/rms/backend/WebConfig.java
@@ -0,0 +1,16 @@
+package com.rms.backend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("*")
+                .allowedHeaders("*");
+    }
+}
